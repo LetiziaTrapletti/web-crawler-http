e00f1b335f84a5c03f0f50410f7349c5f81a83a7
unknown <trapletti.letizia@gmail.com>
2023-12-27 21:04:16 +0000

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody) //jsdom takes html as a string and creates an in-memory object that mirrors the html
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements){
+        if (linkElement.href.slice(0,1) === '/'){
+            //relative
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`) //if the url is invalid, the URl constructor will throw and error
+            urls.push(`${baseURL}${linkElement.href}`)
+            } catch(err){
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            //absolute
+            try{
+                const urlObj = new URL(linkElement.href) //if the url is invalid, the URl constructor will throw and error
+                urls.push(linkElement.href)
+            } catch(err){
+                console.log(`error with absolute url: ${err.message}`)
+            }
+            
+        } 
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}` //remove protocol
@@ -8,5 +37,6 @@ function normalizeURL(urlString) {
 
 //export the function, so i can import it in the crawl.test.js file
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
